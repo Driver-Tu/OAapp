4ab4409402d121bf38e1796e310968edd0d1ccbc
Driver-Tu <3109702282@qq.com>
2024-09-13 17:23:26 +0800

    登录请求 -v1.0.3

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,34 +2,48 @@
 	<view class="content">
 		<image class="logo" src="/static/logo.png"></image>
 		<view class="text-area">
-			{{title}}
-			<el-button>Default</el-button>
-			    <el-button type="primary">Primary</el-button>
-			    <el-button type="success">Success</el-button>
-			    <el-button type="info">Info</el-button>
-			    <el-button type="warning">Warning</el-button>
-			    <el-button type="danger">Danger</el-button>
+			<view class="uni-common-mt">
+			<view class="uni-form-item uni-column">
+							<view class="title">工号/手机号</view>
+							<input class="uni-input" v-model="loginAll.empNum" placeholder="请输入您的工号" />
+						</view>
+			<view class="uni-form-item uni-column">
+							<view class="title">密码</view>
+							<input class="uni-input" v-model="loginAll.password" password type="text" placeholder="请输入您的密码" />
+						</view>
+						<button click="button-login" @click="login">登录</button>
+						<text>{{data}}</text>
+						</view>
+						
 		</view>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+<script setup>
+import axios from 'axios';
+import { reactive, ref } from 'vue';
 
-		},
-		methods: {
-
-		}
-	}
+	const loginAll=reactive({
+		empNum:"",
+		password:""
+	})
+	const data=ref("")
+const login=()=>{
+	axios.post("http://localhost:8088/user/login?empNum="+loginAll.empNum+"&password="+loginAll.password)
+	.then(function(res){
+		console.log(res)
+		data.value=res.data.message
+	}).catch(function(error){
+		console.log(error)
+	})
+}
 </script>
 
 <style>
+	
+	.el-input{
+		margin: 20px auto 20px auto;
+	}
 	.content {
 		display: flex;
 		flex-direction: column;
@@ -53,6 +67,17 @@
 
 	.title {
 		font-size: 36rpx;
-		color: #8f8f94;
+		color: green;
+	}
+	.uni-form-item uni-column{
+		width: 300px;
+		height: 40%;
+		margin: 20px auto 20px auto;
+	}
+	.uni-input{
+		border-bottom: 1px solid green;
+		width: 300px;
+		height: 40%;
+		margin: 20px auto 20px auto;
 	}
 </style>
