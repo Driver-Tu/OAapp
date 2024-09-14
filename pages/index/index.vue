9587c0bfc4833a951602a44aae15f9de70a78818
Driver-Tu <3109702282@qq.com>
2024-09-14 11:49:33 +0800

    登陆页面完成 -v1.0.4

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,7 @@
-<template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
+<template >
+	<view class="content" >
+		<image class="logo" src="/static/logo.png" style="position: fixed;top: 10%;"></image>
+		<view class="text-area" style="position: fixed;top: 45%;">
 			<view class="uni-common-mt">
 			<view class="uni-form-item uni-column">
 							<view class="title">工号/手机号</view>
@@ -11,10 +11,9 @@
 							<view class="title">密码</view>
 							<input class="uni-input" v-model="loginAll.password" password type="text" placeholder="请输入您的密码" />
 						</view>
-						<button click="button-login" @click="login">登录</button>
+						<button class="button-login" click="button-login" @click="login">登录</button>
 						<text>{{data}}</text>
 						</view>
-						
 		</view>
 	</view>
 </template>
@@ -42,9 +41,11 @@ const login=()=>{
 <style>
 	
 	.el-input{
-		margin: 20px auto 20px auto;
+		margin: 50px auto 20px auto;
 	}
 	.content {
+		width: 100%;
+		height: 100%;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
@@ -58,6 +59,7 @@ const login=()=>{
 		margin-left: auto;
 		margin-right: auto;
 		margin-bottom: 50rpx;
+		border-radius: 50px;
 	}
 
 	.text-area {
@@ -79,5 +81,16 @@ const login=()=>{
 		width: 300px;
 		height: 40%;
 		margin: 20px auto 20px auto;
+	}
+	.button-login{
+		background-color: seagreen;
+		color: aliceblue;
+		 border-radius: 25px; 
+	}
+	html, body{
+	  height: 100%;
+	  margin: 0;
+	  padding: 0;
+	  background-color: #f9eee8;
 	}
 </style>
